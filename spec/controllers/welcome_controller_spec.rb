1468fb6e2945ed4677b0367b6d6eee4b629fe84f
DLakin01 <DLakin01@gmail.com>
2017-11-14 09:50:34 -0500

    TDD on Rails working

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe WelcomeController, type: :controller do
     it "renders the index template" do
       get :index
       expect(response).to render_template("index")
-    end  
+    end
+  end
+
+  describe "GET about" do
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template("about")
+    end
   end
 end
