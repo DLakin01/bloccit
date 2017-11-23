2116a4d7fcdd85264c50bfb842a51e50a89a1f7e
DLakin01 <DLakin01@gmail.com>
2017-11-23 13:25:28 -0500

    Created validations and error display for topics

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,10 +3,19 @@ require 'rails_helper'
 RSpec.describe Topic, type: :model do
   let(:name) { RandomData.random_sentence }
   let(:description) { RandomData.random_paragraph }
+  let(:title) { RandomData.random_sentence }
+  let(:body) {RandomData.random_paragraph}
   let(:public) { true }
-  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:topic) { Topic.create!(name: name, description: description, public: public) }
+  let(:post)  { topic.posts.create!(title: title, body: body) }
 
-  it { is_expected.to have_many(:posts) }
+  it { should have_many(:posts) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15) }
 
   describe "attributes" do
     it "has name, description, and public attributes" do
