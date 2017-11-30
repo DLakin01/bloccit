88481006526f2428087e0eb7d44aa4f87a872a0d
DLakin01 <DLakin01@gmail.com>
2017-11-30 10:55:21 -0500

    wip authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   before_save { self.email = email.downcase if email.present? }
 
+  # The following codes uses the "||=" shorthand to check if the user
+  # already has a role. If not, the user is assigned the role of "member"
+  before_save { self.role ||= :member }
+
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
@@ -12,4 +16,8 @@ class User < ApplicationRecord
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  # Maps the given array of attribute types to the integers actually
+  # present in our database. 0 = member, and 1 = admin.
+  enum role: [:member, :admin]
 end
